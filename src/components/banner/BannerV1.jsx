275cbc0c680a179adf7cd747074dc3c755bb6464
Sonawane-Shubham <sssonawane047@gmail.com>
2025-08-14 10:48:45 +0530

    change hero section,remove top menu,remove other social share and keep linkedin only

--- src/components/banner/BannerV1.jsx
+++ src/components/banner/BannerV1.jsx
@@ -21,9 +21,9 @@ const BannerV1 = () => {
   };
 
   const settings = {
-    infinite: true,
+    infinite: false,
     autoplay: true,
-    arrows: true,
+    arrows: false,
     slidesToShow: 1,
     speed: 500,
     slidesToScroll: 1,
